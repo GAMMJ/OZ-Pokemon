061a79c93e7c793a8dbfc0746a5735733a192fe0
GAMMJ <gammjmj@gmail.com>
2026-02-22 18:17:05 +0900

    feat: 디테일 페이지 구현(#7)

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ import { Favorite } from "./pages/Favorite"
 
 function App() {
   return (
-    <div>
+    <div className="bg-[#818181] min-h-screen">
       <div className="bg-red-600 w-full h-14"></div>
       <h1 className="text-4xl text-center bg-black text-white p-2">포켓몬 도감</h1>
-      <nav className="flex gap-3.5 justify-center items-center py-4">
+      <nav className="flex gap-3.5 justify-center items-center py-4 bg-white">
         <Link to={"/"}>메인페이지</Link>
         <Link to={`/detail/1`}>디테일</Link>
         <Link to={"/search"}>서치</Link>
         <Link to={"/favorite"}>찜</Link>
       </nav>
-      <main>
+      <main className="p-4">
         <Routes>
           <Route path="/" element={<Main />}></Route>
           <Route path="/detail/:pokemonId" element={<Detail />}></Route>
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,3 +1,17 @@
+import { useParams } from "react-router"
+import { usePokemonStore } from "../store/pokemonStore"
+
 export const Detail = () => {
-  return <></>
+  const { pokemonId } = useParams()
+  const pokemon = usePokemonStore((state) => state.pokemonList.find((el) => el.id === Number(pokemonId)))
+
+  return (
+    <div className="flex justify-center">
+      <section className="flex flex-col justify-center items-center text-center w-100 h-110 p-5 bg-white rounded-2xl gap-2.5 shadow-[4px_4px_0px_black]">
+        <div className="text-3xl mb-3">{pokemon.name}</div>
+        <div className="whitespace-pre-wrap text-[18px]">{pokemon.description}</div>
+        <img className="w-50 transition" src={pokemon.front} alt={pokemon.name} />
+      </section>
+    </div>
+  )
 }
