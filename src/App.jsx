0b162bee13c3cdd118a93e7ca4523de94ecef86f
GAMMJ <gammjmj@gmail.com>
2026-02-23 11:29:12 +0900

    feat: 서치 페이지 구현(#9)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { Link, Route, Routes } from "react-router"
+import { Link, Route, Routes, useNavigate } from "react-router"
 import { Main } from "./pages/main"
 import { Detail } from "./pages/Detail"
 import { Search } from "./pages/Search"
 import { Favorite } from "./pages/Favorite"
 
 function App() {
+  const navigate = useNavigate()
+
   return (
     <div className="bg-[#818181] min-h-screen">
       <div className="bg-red-600 w-full h-14"></div>
       <h1 className="text-4xl text-center bg-black text-white p-2">포켓몬 도감</h1>
-      <nav className="flex gap-3.5 justify-center items-center py-4 bg-white">
+      <nav className="flex gap-3.5 justify-center items-center py-3 bg-white">
         <Link to={"/"}>메인페이지</Link>
-        <Link to={`/detail/1`}>디테일</Link>
-        <Link to={"/search"}>서치</Link>
         <Link to={"/favorite"}>찜</Link>
+        <div>
+          <input
+            onChange={(e) => navigate(`search?pokemon=${e.target.value}`)}
+            className="border-b p-1.5 outline-none"
+          />
+        </div>
       </nav>
       <main className="p-4">
         <Routes>
