26f2f57b00b915f29ed9a139c3391804e6992e03
GAMMJ <gammjmj@gmail.com>
2026-02-23 14:21:57 +0900

    feat: 찜 목록 페이지 구현(#11)

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,7 +1,10 @@
 import { useNavigate } from "react-router"
+import { useFavoriteStore } from "../store/pokemonFavorite"
 
 export const Card = ({ pokemon }) => {
   const navigate = useNavigate()
+  const toggleFavorite = useFavoriteStore((state) => state.toggleFavorite)
+  const isFavorite = useFavoriteStore((state) => state.pokemonFavorite.some((p) => p.id === pokemon.id))
 
   return (
     <>
@@ -10,7 +13,18 @@ export const Card = ({ pokemon }) => {
         className="flex flex-col justify-center w-37.5 items-center bg-white rounded-2xl gap-2.5 shadow-[4px_4px_0px_black] transition transform hover:-translate-y-1"
       >
         <img className="w-30" src={pokemon.front} alt={pokemon.name} />
-        <div className="pb-2">{pokemon.name}</div>
+        <div className="pb-2">
+          {pokemon.name}
+          <button
+            onClick={(e) => {
+              e.stopPropagation()
+              toggleFavorite(pokemon)
+            }}
+            className={isFavorite ? "text-red-500" : ""}
+          >
+            {isFavorite ? "♥" : "♡"}
+          </button>
+        </div>
       </section>
     </>
   )
