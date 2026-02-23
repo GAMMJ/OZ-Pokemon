367c8654dea83a0cf8b8254f25d0ea6861cdae06
GAMMJ <gammjmj@gmail.com>
2026-02-23 18:26:49 +0900

    feat: 사진 뒤집기 컴포넌트 추가

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router"
 import { usePokemonStore } from "../store/pokemonStore"
 import { useEffect } from "react"
 import { Loading } from "../component/Loading"
+import { FlipImage } from "../component/FlipImage"
 
 const Detail = () => {
   const { pokemonId } = useParams()
@@ -25,10 +26,10 @@ const Detail = () => {
 
   return (
     <div className="flex justify-center">
-      <section className="flex flex-col justify-center items-center text-center w-100 h-110 p-5 text-[#1e1e1e] bg-white rounded-2xl gap-2.5 shadow-[4px_4px_0px_black]">
+      <section className="flex flex-col justify-center items-center text-center w-100 min-h-130 p-5 text-[#1e1e1e] bg-white rounded-2xl gap-2.5 shadow-[4px_4px_0px_black]">
         <div className="text-3xl mb-3">{pokemon.name}</div>
         <div className="whitespace-pre-wrap text-[18px]">{pokemon.description}</div>
-        <img className="w-50 transition" src={pokemon.front} alt={pokemon.name} />
+        <FlipImage front={pokemon.front} back={pokemon.back} />
       </section>
     </div>
   )
